4d866ee26081cb349bda5128439da0e63157e4cc
Joel AZEMAR <joel.azemar@gmail.com>
2015-07-14 10:56:16 +0200

    refactoring spec sign

--- spec/features/sign_in_redirection_spec.rb
+++ spec/features/sign_in_redirection_spec.rb
@@ -3,19 +3,16 @@ require 'acceptance_helper'
 describe 'sign in', type: :feature do
   include MailerHelper
 
-  let(:user) { create(:user_with_vault, :confirmed, subdomain: 'me') }
+  let(:user) do |variable|
+    create(:user_with_vault, :confirmed, subdomain: 'me').tap do |user|
+      expect(user.vault).to be_present
+      expect(user.vault.subdomain).to eql('me')
+    end
+  end
 
   it 'should redirect to show vault' do
-    visit new_user_session_path
-    within("#new_user") do
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: 'secret'
-    end
-    expect(user.vault).to be_present
-    click_button I18n.t('devise.sessions.new.sign_in') # Sign in
+    sign_in_as user, 'secret'
     expect(current_path).to eq("/goldbricks")
-    # expect(current_path).to eq("http://me.example.com/users")
-    # expect(current_path).to eq("http://#{user.vault.subdomain}.example.com#{users_url(subdomain: user.vault.subdomain)}")
   end
 
 end
--- spec/features/sign_up_redirection_spec.rb
+++ spec/features/sign_up_redirection_spec.rb
@@ -4,42 +4,26 @@ describe 'sign up', type: :feature do
   include MailerHelper
 
   let(:email) { Faker::Internet.email }
-
+  let(:user) { double(:user, email: email) }
+  
   it 'should redirect to new vault' do
+    # Create account
+    sign_up_as user, 'secret'
 
-    # create account
-    visit new_user_registration_path
-    within("form#new_user") do
-      fill_in 'user[email]', with: email
-      fill_in 'user[password]', with: 'secret'
-      fill_in 'user[password_confirmation]', with: 'secret'
-    end
-    click_button I18n.t('devise.registrations.new.sign_up') # Sign up
-
-    # Try to connect
+    # Try to visit => Sign in is required
     visit root_url
     expect(current_path).to eq(new_user_session_path)
-    # Try to Sign in
-    within("#new_user") do
-      fill_in 'user[email]', with: email
-      fill_in 'user[password]', with: 'secret'
-    end
-    click_button I18n.t('devise.sessions.new.sign_in') # Sign in
+
+    # Try to Sign in => Confirmation is required
+    fill_login_as user, 'secret'
     expect(page).to have_content(I18n.t('devise.failure.unconfirmed')) # A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.
 
     # Confirm account
-    expect(last_email.to).to have_content email
-    expect(last_email.body).to have_content I18n.t('devise.mailer.confirmation_instructions.action') # Confirm my account
-    link = last_email.body.raw_source.match(/href="(?<url>.+?)">/)[:url]
-    visit link
+    confirm_last_sign_up!
     expect(page).to have_content I18n.t('devise.confirmations.confirmed') # Your email address has been successfully confirmed.
 
-    # Sign in
-    within("#new_user") do
-      fill_in 'user[email]', with: email
-      fill_in 'user[password]', with: 'secret'
-    end
-    click_button I18n.t('devise.sessions.new.sign_in') # Sign in
+    # Sign in => New to create a subdomain
+    fill_login_as user, 'secret'
     expect(current_path).to eq(new_vault_path)
   end
 
